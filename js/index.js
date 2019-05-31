62c96dab4c256a0e2fc2ec6aee7158c09cf0d4bc
yangtao <1509440167@qq.com>
2019-05-31 16:24:52 +0800

    1111

---
+++ js/index.js
@@ -0,0 +1,41 @@
+﻿
+// ios点击事件不触发
+$(function() {  
+    FastClick.attach(document.body);  
+})
+$(document).ready(function () {
+    var str1 = "<p>此功能工程师，</p><p>在努力开发中！</p>"
+    var str2 = "<p>活动暂未开始，</p><p>请您敬请期待！</p>"
+    $('.tc_01').on('click',function(){
+        // 第一个弹窗
+        callPopup(str1);
+    })
+    $('.tc_02').on('click',function(){
+        // 第二个弹窗
+        callPopup(str2);
+    })
+    //调用弹窗
+    // str:弹窗内容
+    function callPopup (str){
+        showMask();
+        $('#tc_text').html(str)
+        $('.tc').show();
+    }    
+    // 关闭弹窗
+    $('.close').on('click',function(){
+        hideMask();
+        $(this).parent().hide()
+    })
+});
+//显示遮罩层
+function showMask() {
+    $("#mask").css("height", $(document).height());
+    $("#mask").css("width", $(document).width());
+    $("#mask").show();
+    $('body').css('position', 'fixed');
+}
+//隐藏遮罩层
+function hideMask() {
+    $("#mask").hide();
+    $('body').css('position', 'unset');
+}
