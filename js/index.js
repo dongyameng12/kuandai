ca0f61c58522eca9cc40b6c6ba76412cd1c01e9d
yangtao <1509440167@qq.com>
2019-06-03 16:28:15 +0800

    222

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ $(function() {
 $(document).ready(function () {
     var str1 = "<p>此功能工程师，</p><p>在努力开发中！</p>"
     var str2 = "<p>活动暂未开始，</p><p>请您敬请期待！</p>"
+    var str3 = "<p>请根据提示音按5</p>"
     $('.tc_01').on('click',function(){
         // 第一个弹窗
         callPopup(str1);
@@ -13,6 +14,10 @@ $(document).ready(function () {
     $('.tc_02').on('click',function(){
         // 第二个弹窗
         callPopup(str2);
+    })
+    $('.tc_03').on('click',function(){
+        // 第三个弹窗
+        callPopup(str3);
     })
     //调用弹窗
     // str:弹窗内容
